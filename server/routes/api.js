a2eee0b52a366c17f8d04f82219ed0eb19531ad4
dhilipanraja-joseph <dlipnraj@gmail.com>
2016-09-26 19:33:27 -0400

    add api routes

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,1 +1,8 @@
+const express = require('express');
+const router = express.Router();
 
+router.use('/users', require('./users'));
+router.use('/books', require('./books'));
+router.use('/classes', require('./classes'));
+
+module.exports = router;
