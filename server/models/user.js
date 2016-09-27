1a849f531d353293df7a023a69a7ba33fee2da95
duhdanube <Danny.B.Lim@gmail.com>
2016-09-27 12:48:09 -0400

    Added more to User schema

--- server/models/user.js
+++ server/models/user.js
@@ -3,8 +3,23 @@ const Schema = mongoose.Schema;
 const passportLocalMongoose = require('passport-local-mongoose');
 
 var User = new Schema({
-  username: {type: String},
-  password: {type: String}
+  username: {type: String, required: true},
+  password: {type: String, required: true},
+  firstName: {type: String, required: true},
+  lastName: {type: String, required: true},
+  email: {
+        type: String,
+        trim: true,
+        lowercase: true,
+        unique: true,
+        required: 'Email address is required',
+        validate: [validateEmail, 'Please fill a valid email address'],
+        match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
+  },
+  phone: {type: Number, required: true},
+  books: {type: Schema.Types.ObjectId, ref: 'Book'},
+  favorites: {type: Schema.Types.ObjectId, ref: 'Book'},
+  cart: {type: Schema.Types.ObjectId, ref: 'Book'}
 });
 
 User.plugin(passportLocalMongoose);
