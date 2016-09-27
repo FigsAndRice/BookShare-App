25b054dccb9288013b56ceda841df2cc5fdfe4e5
Juan Carlos <juancafe2@gmail.com>
2016-09-27 14:09:21 -0400

    Add store.js

---
+++ public/store.js
@@ -0,0 +1,14 @@
+import { createStore, applyMiddleware, compose } from 'redux'
+import thunk from 'redux-thunk'
+import logger from 'redux-logger'
+
+import reducers from './reducers'
+
+const middleware = applyMiddleware(thunk, logger());
+
+const store = createStore(reducers, compose(
+    middleware,
+    window.devToolsExtension ? window.devToolsExtension() : f => f
+  ));
+
+export default store;
