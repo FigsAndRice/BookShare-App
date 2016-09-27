86caffefba17a7ae9adb22948b3247cb0c06ea8e
Juan Carlos <juancafe2@gmail.com>
2016-09-27 17:10:47 -0400

    Added Results component

---
+++ public/components/Results.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {List, ListItem} from 'material-ui';
+class Results extends React.Component {
+    constructor(props) {
+        super(props);
+        this.displayName = 'Results';
+    }
+    render() {
+        return (
+        	<List>
+        		
+        	</List>
+        );
+    }
+}
+
+export default Results;
