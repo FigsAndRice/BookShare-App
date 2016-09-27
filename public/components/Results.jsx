31113f9c74fc57381e8bfbd5fe6f0de3debf21fb
Juan Carlos <juancafe2@gmail.com>
2016-09-27 17:48:22 -0400

    Adding Results info routes

--- public/components/Results.jsx
+++ public/components/Results.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
-import {List, ListItem} from 'material-ui';
+import {List, ListItem, Avatar} from 'material-ui';
 class Results extends React.Component {
     constructor(props) {
         super(props);
         this.displayName = 'Results';
     }
     render() {
+    		let img = <img src="http://books.google.com/books/content?id=RJxWIQOvoZUC&printsec=frontcover&img=1&zoom=5&edge=curl&imgtk=AFLRE71ema0EayCQkQ7HS2nqnUVvLP_t80kSWA011JCg8atZRt7cG3cN8Sxm-fB3eA5di9GXySS2MWFHKbEnz4yBmjFJOnaVykxstmbqPKuNqTccj1GRCaj4wcAy5bPIy6dyvy6RItY8&source=gbs_api" />
         return (
         	<List>
-        		
+        		<ListItem leftIcon={img} primaryText={"Title: Flowers"} 
+        		secondaryText={"Author: Something \n\n ISBN:3435435345"}> </ListItem>
         	</List>
         );
     }
--- public/index.js
+++ public/index.js
@@ -8,6 +8,7 @@ import { MuiThemeProvider } from 'material-ui';
 
 import App from './components/App.jsx';
 import Main from './components/Main.jsx';
+import Results from './components/Results.jsx';
 import NotFound from './components/NotFound.jsx';
 
 //import store from './store';
@@ -20,6 +21,7 @@ render(
       <Router history={browserHistory}>
         <Route path="/" component={App}>
         	<IndexRoute component={Main} />
+        	<Route path="/results" component={Results} />
         </Route>
         <Route path="*" component={NotFound} />
       </Router>
