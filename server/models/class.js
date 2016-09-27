c18c972942913f1148583eca7a61c5c994764f42
dhilipanraja-joseph <dlipnraj@gmail.com>
2016-09-26 20:35:06 -0400

    add classes routes

---
+++ server/models/class.js
@@ -0,0 +1,12 @@
+
+const mongoose = require('mongoose');
+
+const classSchema = new mongoose.Schema({
+  class_name : {type: String, required:true},
+  department : {type: String, required:true},
+  section : {type : String, required:true}
+});
+
+const Class = mongoose.model('Class', classSchema);
+
+module.exports = Class;
---
+++ server/routes/classes.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const router = express.Router();
+
+const Class = require('../models/class');
+
+router.route('/')
+      .get((req,res)=>{
+        Class.find({},(err,courses)=>{
+          res.status(err ? 400 : 200).send(err || courses);
+        })
+      })
+      .post((req,res)=>{
+        Class.create(req.body,(err, course)=>{
+          res.status(err ? 400 : 200).send(err || course);
+        })
+      })
+
+router.route('/:id')
+      .get((req,res)=>{
+        Class.find(req.params.id,(err, course)=>{
+          res.status(err ? 400 : 200).send(err || course);
+        })
+      })
+      .put((req,res)=>{
+        Class.findByIdAndUpdate(req.params.id,{$set : req.body},{new : true},(err,course)=>{
+         if(err){
+           res.status(400).send(err);
+         }else{
+           res.status(err ? 400 : 200).send( err || course);
+         }
+       })
+      })
+      .delete((req,res)=>{
+        Class.findByIdAndRemove(req.params.id,(err, course)=>{
+          res.status(err ? 400 : 200).send(err || course.class_name+" deleted");
+        })
+      })
+
+
+module.exports = router;
