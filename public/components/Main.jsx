dfb4cad570df8c912f0782f105199c430186b935
Hyein <hyeinu65@gmail.com>
2016-09-28 16:53:10 -0400

    started working on the splash page

--- public/components/Main.jsx
+++ public/components/Main.jsx
@@ -1,14 +1,19 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-class Main extends React.Component {
+class Main extends Component {
     constructor(props) {
         super(props);
         this.displayName = 'Main';
-        
+
     }
     render() {
         return <h1>This is the Splash Page</h1>
     }
 }
 
-export default Main;
+
+export default connect(state => ({
+  user: state.user
+  }),dispatch => {
+  })(Main);
