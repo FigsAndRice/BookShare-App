00dfab23909150b1bb03e359efba00f8c632ae20
Hyein <hyeinu65@gmail.com>
2016-09-28 19:04:42 -0400

    fixed the UserActions and routes

---
+++ public/components/library/Book.jsx
@@ -0,0 +1,15 @@
+import React , { Component } from 'react'
+import { RaisedButton } from 'material-ui'
+
+export default class Book extends Component{
+  render(){
+    return(
+    <div className="col-sm-4 col-md-4 col-lg-4 bookbox">
+      <img width="150px" src="http://1615.info/images/red-book.jpg" alt="NO_IMG"/>
+      <div>Book Name</div>
+        <RaisedButton>Buy</RaisedButton>
+        <RaisedButton>Edit</RaisedButton>
+      </div>
+    )
+  }
+}
