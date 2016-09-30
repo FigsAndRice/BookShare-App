3e32f9b156d778bfef04c255ef026dd8dd88e480
Hyein <hyeinu65@gmail.com>
2016-09-30 11:30:25 -0400

    changed some eslint errors in models/routes book

--- server/models/book.js
+++ server/models/book.js
@@ -1,19 +1,20 @@
 const mongoose = require('mongoose');
+
 const Schema = mongoose.Schema;
 
 const bookSchema = new mongoose.Schema({
-  title: {type: String, required: true},
-  author: {type: String, required: true},
-  cover: {type: String, required: true},
+  title: { type: String, required: true },
+  author: { type: String, required: true },
+  cover: { type: String, required: true },
   isbn: { type: String, required: true },
   price: { type: Number },
   condition: { type: String },
   time: { type: Date, default: Date.now },
-  forSale: { type: Boolean, default: false},
-  owner: { type: Schema.Types.ObjectId, ref: 'User', required: true},
+  forSale: { type: Boolean, default: false },
+  owner: { type: Schema.Types.ObjectId, ref: 'User', required: true },
   picture: [{ type: Schema.Types.ObjectId, ref: 'Image' }],
   class_name: { type: Schema.Types.ObjectId, ref: 'Class'}
-})
+});
 
 const Book = mongoose.model('Book', bookSchema);
 
--- server/routes/books.js
+++ server/routes/books.js
@@ -1,4 +1,5 @@
 const express = require('express');
+
 const router = express.Router();
 const multer = require('multer');
 
@@ -9,61 +10,66 @@ const upload = multer({ storage: multer.memoryStorage() });
 
 router.get('/', (req, res) => {
   Book.find({}, (err, books) => {
-    res.status(err ? 400 : 200).send(err || books)
-  }).populate('owner')
-})
+    res.status(err ? 400 : 200).send(err || books);
+  }).populate('owner');
+});
 
 router.post('/', (req, res) => {
   Book.create(req.body, (err, book) => {
     res.status(err ? 400 : 200).send(err || book);
-  })
-})
+  });
+});
 
 router.get('/:isbn', (req, res) => {
   Book.find({ isbn: req.params.isbn }, (err, books) => {
-    res.status(err ? 400 : 200).send(err || books)
-  }).populate('owner')
-})
+    res.status(err ? 400 : 200).send(err || books);
+  }).populate('owner');
+});
 
 router.get('/owner/:ownerid', (req, res) => {
   Book.find({ owner: req.params.ownerid }, (err, books) => {
-    res.status(err ? 400 : 200).send(err || books)
-  })
-})
+    res.status(err ? 400 : 200).send(err || books);
+  });
+});
 
-router.delete('/:id', (req, res) =>{
+router.delete('/:id', (req, res) => {
   Book.findByIdAndRemove(req.params.id, err => {
-    if(err) return res.status(400).send(err)
-    res.send()
-  })
-})
+    if (err) return res.status(400).send(err);
+    res.send();
+  });
+});
 
-router.put('/:bookid/addImage', upload.single('image'), (req, res) =>{
+router.put('/:bookid/addImage', upload.single('image'), (req, res) => {
   Image.upload(req.file, (err, photo) => {
-    if (err) return res.status(400).send(err)
+    if (err) return res.status(400).send(err);
     Book.findById(req.params.bookid, (err, book) => {
-      if(err || !book){
-        return res.status(400).send(err || 'Book not Found!')
+      if (err || !book) {
+        return res.status(400).send(err || 'Book not Found!');
       }
       book.pictures.push(photo._id);
-      book.save(err =>{
+      book.save(err => {
+        if (err) res.status(400).send(err);
         res.send();
-      })
-    }).populate('owner')
-  })
-})
+      });
+    }).populate('owner');
+  });
+});
 
 router.put('/:bookid/changeOwner/:ownerid', (req, res) => {
-  Book.findByIdAndUpdate(req.params.bookid, { $set: { owner: req.params.ownerid } }, { new: true }, (err, book) => {
-    res.status(err ? 400 : 200).send(err || book);
-  })
-})
+  Book.findByIdAndUpdate(req.params.bookid,
+    { $set: { owner: req.params.ownerid } },
+    { new: true }, (err, book) => {
+      res.status(err ? 400 : 200).send(err || book);
+      });
+});
 
-router.put('/:bookid/:classid', (req, res) =>{
-  Book.findByIdAndUpdate(req.params.bookid, { $set : { class_name: req.params.classid } }, { new : true }, (err,book) => {
-   res.status(err ? 400 : 200).send(err || book)
- })
-})
+router.put('/:bookid/:classid', (req, res) => {
+  Book.findByIdAndUpdate(req.params.bookid,
+    { $set: { class_name: req.params.classid } },
+    { new: true }, (err,book) => {
+      res.status(err ? 400 : 200).send(err || book);
+      });
+});
 
 router.delete('/:bookid/:imageid', Image.RemoveMiddleware, (req , res) => {
   Image.findOneById({ _id: req.params.imageid })
@@ -73,28 +79,28 @@ router.delete('/:bookid/:imageid', Image.RemoveMiddleware, (req , res) => {
           { _id: req.params.bookid },
           { $pull: { pictures: req.params.imageid } },
           { new: true },
-          function(err, removedFromBook) {
-            if (err) res.status(400).send(err)
-            res.status(200).send()
+          (err, removedFromBook) => {
+            if (err) res.status(400).send(err);
+            res.status(200).send();
           }
-        )
-      })
-    })
-})
+        );
+      });
+    });
+});
 
 router.delete('/:bookid', (req, res) => {
   Book.findOneById(req.params.bookid, (err, book) => {
     book.pictures.map(pic => {
       Image.findOneAndRemove({ _id: pic }, Image.RemoveMiddlware, err => {
-        if(err) return err;
+        if (err) return err;
         return;
-      })
-    })
+      });
+    });
     Book.findOneByIdAndRemove(req.params.bookid, err => {
       if (err) res.status(400).send(err);
-      res.status(200).send()
-    })
-  })
-})
+      res.status(200).send();
+    });
+  });
+});
 
 module.exports = router;
