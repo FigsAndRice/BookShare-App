a087ae725d2b8778d2be61750ecb4e94e081d409
Juan Carlos <juancafe2@gmail.com>
2016-10-02 19:54:14 -0400

    Fix props and reducer setting to null book in default

--- public/components/booksearch/Book.jsx
+++ public/components/booksearch/Book.jsx
@@ -8,7 +8,7 @@ import CommunicationChatBubble from 'material-ui/svg-icons/communication/chat-bu
 import StarBorder from 'material-ui/svg-icons/toggle/star-border';
 
 import { addToCart, addFavorite, getUser, receiveUser} from '../../actions/UserActions';
-import { addBook, searchBooks, getBook } from '../../actions/BookActions';
+import { addBook, removeFromCart, searchBooks, getBook } from '../../actions/BookActions';
 
 const styles = {
   bookCover: {
@@ -34,127 +34,154 @@ class Book extends Component {
     super(props);
 
     this.state = {
-      searchedBooks: this.props.searchedBooks
+      searchedBooks: null,
     }
 
     this._addBook = this._addBook.bind(this);
     this._addToCart = this._addToCart.bind(this);
     this._addFavorite = this._addFavorite.bind(this);
+    this._updateCart = this._updateCart.bind(this);
+    this._removeFromCart = this._removeFromCart.bind(this);
   }
 
   componentDidMount() {
-    let user = JSON.parse(localStorage.user)
-    let book = JSON.parse(localStorage.book)
+    let user = JSON.parse(localStorage.user);
+    let book = JSON.parse(localStorage.book);
     this.props.receiveUser(user)
     this.props.searchBooks(this.props.params.isbn);
     this.props.getBook(book);
+  }
 
-    let searchBooks = this.props.searchedBooks.map(book => {
-      book.addToCart = false;
-      return book;
-    }); 
-    console.log('updated books ', searchBooks)
-    this.setState({
-      searchedBooks: this.props.searchBooks,
-    });
+  componentWillReceiveProps(props) {
+    if (props.search ) {
+      let searchBooks = props.search.map(book => {
+        book.addToCart = false;
+        return book;
+      }); 
+      this.setState({
+        searchedBooks: searchBooks,
+      });   
+    }
   }
 
   _addBook() {
     this.props.addBook(this.props.book, this.props.user._id);
   }
+  _updateCart(book) {
+    if (book.addToCart) {
 
-  _addToCart(e) {
-    this.props.addToCart(this.props.user._id, e.target.dataset.bookid);
+    }
+    else {
+
+    }
+  }
+  _addToCart(id, bookId) {
+    this.props.addToCart(this.props.user._id, bookId);
   }
 
+  _removeFromCart(id, bookId) {
+    this.props.removeFromCart(this.props..user._id, bookId);
+  }
   _addFavorite(e) {
     this.props.addFavorite(this.props.user._id, e.target.dataset.bookid);
   }
-
   render() {
-    let { book, searchedBooks } = this.props;
+    if (this.props.search && this.state.searchedBooks) {
+      let { book, searchedBooks } = this.props;
 
-    const userBooks = searchedBooks.map((existingBook, index) => {
-      let bookPicture;
-      if (existingBook.picture.length) {
-        bookPicture = <img src={existingBook.picture} className="img-responsive" style={styles.gridTile}/>
-      } else {
-        bookPicture = <img src={book.pictureNormal} className="img-responsive" style={styles.gridTile}/>
-      }
+      const userBooks = this.state.searchedBooks.map((existingBook, index) => {
+        let bookPicture;
+        if (existingBook.picture.length) {
+          bookPicture = <img src={existingBook.picture} className="img-responsive" style={styles.gridTile}/>
+        } else {
+          bookPicture = <img src={book.pictureNormal} className="img-responsive" style={styles.gridTile}/>
+        }
+
+
+        //addToCart -> true 
+        let btnAction = 'remove_shopping_cart';
+    
+        if (!existingBook.addToCart) {
+           btnAction = 'add_shopping_cart';
+        }
+
+        return (
+        <ListItem key={index}>
+          <div className="row">
+            <div className="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+              {bookPicture}
+            </div>
+            <div className="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+              <h3>Owned by: {existingBook.owner.username}</h3>
+              <h3>Email: {existingBook.owner.email}</h3>
+            </div>
+            <div className="col-xs-4 col-sm-4 col-md-4 col-lg-4 text-right">
+              <FloatingActionButton iconStyle={{color: "#FBC02D "}}>
+                <FontIcon className='material-icons' onClick={this._updateCart.bind(null, existingBook)} data-bookid={existingBook._id}>{btnAction}</FontIcon>
+              </FloatingActionButton>
+              <FloatingActionButton iconStyle={{color: "#FBC02D "}}>
+                <FontIcon className='material-icons' onClick={this._addFavorite} data-bookid={existingBook._id}>favorite</FontIcon>
+              </FloatingActionButton>
+            </div>
+          </div>
+        </ListItem>
+        )
+      })
 
       return (
-      <ListItem key={index}>
-        <div className="row">
-          <div className="col-xs-4 col-sm-4 col-md-4 col-lg-4">
-            {bookPicture}
-          </div>
-          <div className="col-xs-4 col-sm-4 col-md-4 col-lg-4">
-            <h3>Owned by: {existingBook.owner.username}</h3>
-            <h3>Email: {existingBook.owner.email}</h3>
-          </div>
-          <div className="col-xs-4 col-sm-4 col-md-4 col-lg-4 text-right">
-            <FloatingActionButton iconStyle={{color: "#FBC02D "}}>
-              <FontIcon className='material-icons' onClick={this._addToCart} data-bookid={existingBook._id}>add</FontIcon>
-            </FloatingActionButton>
-            <FloatingActionButton iconStyle={{color: "#FBC02D "}}>
-              <FontIcon className='material-icons' onClick={this._addFavorite} data-bookid={existingBook._id}>favorite</FontIcon>
-            </FloatingActionButton>
-          </div>
-        </div>
-      </ListItem>
-      )
-    })
-
-    return (
-      <div>
-        <div className="container">
-          <div className="row" style={{borderStyle: "solid", borderColor: amber600}}>
-            <div style={{paddingTop: "20px"}}className="col-sm-2 col-md-2 col-md-offset-1 col-xs-offset-4">
-              <img src={book.pictureNormal} className="img-responsive"/>
-              <br />
-              <br />
-              <RaisedButton
-                onClick={this._addBook}
-                label="Add Book"
-                primary={false}
-                style={{float: "right"}}
-                labelColor={yellow600}
-                backgroundColor={lightBlue900}
-                icon={<FontIcon className="material-icons">add</FontIcon>}
-              />
-            </div>
-            <div className="col-md-9 col-sm-6">
-              <div >
-                <h1 className="text-center"><b>{book.title}</b></h1>
-                <br/>
-                <h4> <i>{book.description}</i></h4>
-                <br/>
-                <h4>Author(s): {book.authors}</h4>
-                <h4>ISBN: {book.isbn}</h4>
+        <div>
+          <div className="container">
+            <div className="row" style={{borderStyle: "solid", borderColor: amber600}}>
+              <div style={{paddingTop: "20px"}}className="col-sm-2 col-md-2 col-md-offset-1 col-xs-offset-4">
+                <img src={book.pictureNormal} className="img-responsive"/>
+                <br />
+                <br />
+                <RaisedButton
+                  onClick={this._addBook}
+                  label="Add Book"
+                  primary={false}
+                  style={{float: "right"}}
+                  labelColor={yellow600}
+                  backgroundColor={lightBlue900}
+                  icon={<FontIcon className="material-icons">add</FontIcon>}
+                />
+              </div>
+              <div className="col-md-9 col-sm-6">
+                <div >
+                  <h1 className="text-center"><b>{book.title}</b></h1>
+                  <br/>
+                  <h4> <i>{book.description}</i></h4>
+                  <br/>
+                  <h4>Author(s): {book.authors}</h4>
+                  <h4>ISBN: {book.isbn}</h4>
+                </div>
               </div>
             </div>
           </div>
+          <div>
+            <br/>
+            <AppBar showMenuIconButton={false} title="Sale by Other Students"/>
+            <List>
+              {userBooks}
+            </List>
+          </div>
         </div>
-        <div>
-          <br/>
-          <AppBar showMenuIconButton={false} title="Sale by Other Students"/>
-          <List>
-            {userBooks}
-          </List>
-        </div>
-      </div>
-    )
+      )
+    }
+    else return <div></div>
   }
+
 }
 
 export default connect(state => ({
     book: state.books.book,
     user: state.user,
-    searchedBooks: state.searchedBooks
+    search: state.searchedBooks
   }),
   dispatch => {
     return {
       addToCart: (userId, bookId) => {dispatch(addToCart(userId, bookId))},
+      removeFromCart: (userId, bookId) => {dispatch(removeFromCart(userId, bookId))},
       addFavorite: (userId, bookId) => {dispatch(addFavorite(userId, bookId))},
       addBook: (book, userId) => {dispatch(addBook(book, userId))},
       getUser: id => {dispatch(getUser(id))},
